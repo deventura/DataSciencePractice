df0a848547cb47aa9902ee22b3867bcdabb82389
Denisse Ventura <d.ventura@ufl.edu>
2018-09-19 20:35:54 -0400

    Commit

--- Code.R
+++ Code.R
@@ -6,6 +6,6 @@ str(wage1)
 plot(wage1$wage, wage1$numdep, main="Wage vs. Dependence", 
      xlab="wage ", ylab="dependence ", pch=19)
 abline(lm(data = wage1, wage ~ numdep))
-my_model = lm(data = wage1, wage.numdep)
+my_model = lm(data = wage1, wage~numdep)
 my_model
 plot(my_model)
