382bd04c09299360226e1dea2c9e81638b19e435
Denisse Ventura <d.ventura@ufl.edu>
2018-09-19 20:25:40 -0400

    Commit

---
+++ Code.R
@@ -0,0 +1,11 @@
+install.packages("wooldridge")
+library(wooldridge)
+data('wage1')
+wage1
+str(wage1)
+plot(wage1$wage, wage1$numdep, main="Wage vs. Dependence", 
+     xlab="wage ", ylab="dependence ", pch=19)
+abline(lm(data = wage1, wage ~ numdep))
+my_model = lm(data = wage1, wage.numdep)
+my_model
+plot(my_model)
